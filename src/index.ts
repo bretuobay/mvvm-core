2a109a8280141b58367123fdac9e94202503a87b
Festus Yeboah <festus.yeboah@konux.de>
2025-05-25 22:45:14 +0200

    Prep for publication

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ export * from "./commands/Command";
 export * from "./models/BaseModel";
 export * from "./models/RestfulApiModel";
 export * from "./viewmodels/BaseViewModel";
+
+export type { Fetcher } from "./models/RestfulApiModel";
+export type { ICommand } from "./commands/Command";
+export type { IObservableCollection } from "./collections/ObservableCollection";
+export type { IBaseModel } from "./models/BaseModel";
